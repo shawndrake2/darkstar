6d9714d69f996a908054e6e23dd5945aef8cf8d9
Drake <shawndrake2@gmail.com>
2018-08-31 17:08:49 -0400

    add update for serverBuild variable timestamp

--- sql/zzz_updateCustom.sql
+++ sql/zzz_updateCustom.sql
@@ -10,3 +10,10 @@ UPDATE `item_usable` SET reuseDelay = 300 WHERE itemid = 27556;
 /*!40000 ALTER TABLE `item_usable` ENABLE KEYS */;
 UNLOCK TABLES;
 
+LOCK TABLES `server_variables` WRITE;
+/*!40000 ALTER TABLE `server_variables` DISABLE KEYS */;
+INSERT INTO `server_variables`(name, value)
+VALUES('serverBuild', UNIX_TIMESTAMP())
+ON DUPLICATE KEY UPDATE value = UNIX_TIMESTAMP();
+/*!40000 ALTER TABLE `server_variables` ENABLE KEYS */;
+UNLOCK TABLES;
